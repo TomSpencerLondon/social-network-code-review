f2158401853340a9c499857f6eee9baf31cafcd5
Tom <thomas.spencer@codurance.com>
2020-08-25 13:13:39 +0100

    Refactor naming for field in CommandDispatcher. It's confusing to have a class member with the same name (case differences aside) as its enclosing class. Also replace with diamond operator

--- src/main/java/it/voxsim/command/CommandDispatcher.java
+++ src/main/java/it/voxsim/command/CommandDispatcher.java
@@ -3,26 +3,21 @@ package it.voxsim.command;
 import java.util.HashMap;
 import java.util.Map;
 
-import it.voxsim.command.Command;
-import it.voxsim.command.FollowCommand;
-import it.voxsim.command.PostCommand;
-import it.voxsim.command.ReadCommand;
-import it.voxsim.command.WallCommand;
 import it.voxsim.repository.LinkRepository;
 import it.voxsim.repository.MessageRepository;
 
 public class CommandDispatcher {
-	private Map<String, Command> commandDispatcher;
+	private Map<String, Command> commands;
 
 	public CommandDispatcher(MessageRepository messageRepository, LinkRepository linkRepository) {
-		commandDispatcher = new HashMap<String, Command>();
-		commandDispatcher.put(null, ReadCommand.create(messageRepository));
-		commandDispatcher.put("->", new PostCommand(messageRepository, linkRepository));
-		commandDispatcher.put("follows", new FollowCommand(linkRepository));
-		commandDispatcher.put("wall", WallCommand.create(messageRepository, linkRepository));
+		commands = new HashMap<>();
+		commands.put(null, ReadCommand.create(messageRepository));
+		commands.put("->", new PostCommand(messageRepository, linkRepository));
+		commands.put("follows", new FollowCommand(linkRepository));
+		commands.put("wall", WallCommand.create(messageRepository, linkRepository));
 	}
 
 	public Command dispatch(String action) {
-		return commandDispatcher.get(action);
+		return commands.get(action);
 	}
 }
